ba998e7dd9adc239e912cc1b2f91b9cbf80315ce
Pavel <alkatrazzzzz@mail.ru>
2021-08-03 22:51:13 +0300

    v2.1

--- src/ru/gb/Main.java
+++ src/ru/gb/Main.java
@@ -8,17 +8,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-	Dog dog=new Dog(5, "Вася");
-	Cat cat = new Cat(3, "Флоппа");
+        Scanner sc = new Scanner(System.in);
+        Dog dog = new Dog(5, "Вася");
+        Cat cat = new Cat(3, "Флоппа");
         System.out.println("Сколько проплывёт собака?");
-    dog.swim(sc.nextInt());
-        System.out.println("Сколько должна пробежать собака " +dog.getName()+ " ?");
+        dog.swim(sc.nextInt());
+        System.out.println("Сколько должна пробежать собака " + dog.getName() + " ?");
         dog.run(sc.nextInt());
-        System.out.println("Сколько должна пробежать кошка " +cat.getName()+ " ?");
+        System.out.println("Сколько должна пробежать кошка " + cat.getName() + " ?");
         cat.run(sc.nextInt());
         System.out.println("Сколько проплывёт кошка?");
         cat.swim(sc.nextInt());
-        System.out.println("Всего: "+ dog.getCounter()+ " собак и " + cat.getCounter() + " котов");
+        System.out.println("Всего: " + dog.getCounter() + " собак и " + cat.getCounter() + " котов");
     }
 }
