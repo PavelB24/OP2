470c9a75913527c7aa8ba9661e328b1120af1c3c
Pavel <alkatrazzzzz@mail.ru>
2021-08-03 22:51:23 +0300

    v2.1

--- src/Animals/Animal.java
+++ src/Animals/Animal.java
@@ -3,14 +3,15 @@ package Animals;
 public abstract class Animal {
     protected int age;
     protected String name;
-    public Animal(int age, String name){
-    this.age=age;
-    this.name=name;
+
+    public Animal(int age, String name) {
+        this.age = age;
+        this.name = name;
     }
 
     public abstract void swim(int meters);
 
     public abstract void run(int meters);
 
-    }
+}
 
